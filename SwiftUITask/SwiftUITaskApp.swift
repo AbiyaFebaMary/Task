ca9c204dbd7390dd055912d82455b00cf90e9464
Febira <abiyafeba08@gmail.com>
2025-05-28 23:02:29 +0530

    Initial Commit

---
+++ SwiftUITask/SwiftUITaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITaskApp.swift
+//  SwiftUITask
+//
+//  Created by AbiyaFeba on 28/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUITaskTests/SwiftUITaskTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITaskTests.swift
+//  SwiftUITaskTests
+//
+//  Created by AbiyaFeba on 28/05/25.
+//
+
+import Testing
+@testable import SwiftUITask
+
+struct SwiftUITaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
